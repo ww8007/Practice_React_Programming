7af1e80a37911658a30af7f3148699702a7b0775
ww8007 <wshmin@naver.com>
2021-06-07 02:41:21 +0900

    [study] 간단한 && 연산자 예시

--- ch04_리액트_실전_활용법/study/src/App.js
+++ ch04_리액트_실전_활용법/study/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [event, isEvent] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {event && <div>이벤트 중</div>}
+      {!event && <div>이벤트 중이 아님</div>}
+      <button onClick={() => isEvent(!event)}>클릭</button>
     </div>
   );
 }
